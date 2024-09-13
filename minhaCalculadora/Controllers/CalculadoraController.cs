d5b2a46dcfbafc977cd1fbf771c9f38e330598cf
gustavolivera <social.gustavosilva@gmail.com>
2024-09-13 14:37:14 -0300

    Add project files.

---
+++ minhaCalculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace minhaCalculadora.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Somar()
+        {
+
+            return View();
+        }
+    }
+}
